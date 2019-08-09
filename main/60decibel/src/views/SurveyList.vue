b3d012e6e21009ee84f7bc057308263aa51edc77
antlikelion <ignorant_woo@naver.com>
2019-08-10 00:28:50 +0900

    아이고야

---
+++ main/60decibel/src/views/SurveyList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="survey-history">
+    <ul>
+      <router-link :to="`/stomach/${item.id}`" v-for="item in survey_history">
+        <span @click="toDetail(item.id)">
+          번호 : {{ item.id }}
+          증상 : {{ item.symptom }}
+          <small>작성일 : {{ item.created_at}}</small>
+        </span>
+      </router-link>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(["survey_history"])
+  },
+  methods: {
+    toDetail(id) {
+      let Id = id;
+      this.push({ path: `/stomach/${Id}` });
+    }
+  },
+  created() {
+    this.$store.dispatch("getSurveyHistory");
+  }
+};
+</script>
+
+<style>
+</style>
